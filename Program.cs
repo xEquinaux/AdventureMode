3544209b07d986197ee805c54a547167b60fbc2e
Nolan Flammini <nolantheturtle@gmail.com>
2024-11-06 11:56:59 -0800

    * added a very tedious set of string values for each waypoint * iterated resulting List<string> with +1 in area value to avoid the 0 index

--- Program.cs
+++ Program.cs
@@ -90,23 +90,21 @@
 		public static string Location(Act act)
 		{
 			int num = rand.Next(8);
-			var getAct = GetAct((int)act);
 			if (act == Act.Four)
 			{
 				num = rand.Next(2);
 			}
-			return GetName(getAct[num], (int)act);
+			return GetName(num + 1, (int)act);
 		}
 		public static string[] Location(Act act, short count = 3)
 		{
 			if (count > 8) count = 8;
 			string[] location = new string[count];
 			int num = 0;
-			var getAct = GetAct((int)act);
 			do
 			{
 				START:
-				string area = GetName(getAct[rand.Next(9)], (int)act);
+				string area = GetName(rand.Next(8) + 1, (int)act);
 				if (!location.Contains(area))
 				{
 					location[num] = area;
@@ -115,64 +113,70 @@
 			} while (num++ < count);
 			return location;
 		}
-		public static List<Waypoint> GetAct(int act)
+		public static string GetName(int num, int act)
 		{
-			return Enum.GetValues<Waypoint>().Where(t => (int)t == act).ToList();
-		}
-		public static string GetName(Waypoint w, int act)
-		{
-			if ((int)w == act && (int)w > 0)
-			{
-				return w.ToString().Replace('_', ' ');
-			}
-			else return string.Empty;
+			return Waypoint.Area(act)[num].Replace('_', ' ');
 		}
 	}
-	public enum Waypoint : int
+	public struct Waypoint
 	{
-		Rogue_Encampment = -1,
-		Cold_Plains = 1,
-		Stony_Field = 1,
-		Dark_Wood = 1,
-		Black_Marsh = 1,
-		Outer_Cloister = 1,
-		Jail, _level_1 = 1,
-		Inner_Cloister = 1,
-		Catacombs_level_2 = 1,
-
-		Lut_Gholein = -2,
-		Sewers_level_2 = 2,
-		Dry_Hills = 2,
-		Halls_of_the_Dead_level_2 = 2,
-		Far_Oasis = 2,
-		Lost_City = 2,
-		Palace_Cellar_level_1 = 2,
-		Arcane_Sanctuary = 2,
-		Canyon_of_the_Magi = 2,
-
-		Kurast_Docks = -3,
-		Spider_Forest = 3,
-		Great_Marsh = 3,
-		Flayer_Jungle = 3,
-		Lower_Kurast = 3,
-		Kurast_Bazaar = 3,
-		Upper_Kurast = 3,
-		Travincal = 3,
-		Durance_of_Hate_level_2 = 3,
-
-		Pandemonium_Fortress = -4,
-		City_of_the_Damned = 4,
-		River_of_Flames = 4,
-
-		Harrogath = -5,
-		Frigid_Highlands = 5,
-		Arreat_Plateau = 5,
-		Crystalline_Passage = 5,
-		Halls_of_Pain = 5,
-		Glacial_Trail = 5,
-		Frozen_Tundra = 5,
-		The_Ancients_Way = 5,
-		Worldstone_Keep_level_2 = 5
+		public static IList<string> Area(int act)
+		{
+			IList<string> result = new List<string>();
+			switch (act)
+			{
+				case 1:
+					result.Add("Rogue_Encampment");
+					result.Add("Cold_Plains");
+					result.Add("Stony_Field");
+					result.Add("Dark_Wood");
+					result.Add("Black_Marsh");
+					result.Add("Outer_Cloister");
+					result.Add("Jail_level_1");
+					result.Add("Inner_Cloister");
+					result.Add("Catacombs_level_2");
+					break;
+				case 2:
+					result.Add("Lut_Gholein");
+					result.Add("Sewers_level_2");
+					result.Add("Dry_Hills");
+					result.Add("Halls_of_the_Dead_level_2");
+					result.Add("Far_Oasis");
+					result.Add("Lost_City");
+					result.Add("Palace_Cellar_level_1");
+					result.Add("Arcane_Sanctuary");
+					result.Add("Canyon_of_the_Magi");
+					break;
+				case 3:
+					result.Add("Kurast_Docks");
+					result.Add("Spider_Forest");
+					result.Add("Great_Marsh");
+					result.Add("Flayer_Jungle");
+					result.Add("Lower_Kurast");
+					result.Add("Kurast_Bazaar");
+					result.Add("Upper_Kurast");
+					result.Add("Travincal");
+					result.Add("Durance_of_Hate_level_2");
+					break;
+				case 4:
+					result.Add("Pandemonium_Fortress");
+					result.Add("City_of_the_Damned");
+					result.Add("River_of_Flames");
+					break;
+				case 5:
+					result.Add("Harrogath");
+					result.Add("Frigid_Highlands");
+					result.Add("Arreat_Plateau");
+					result.Add("Crystalline_Passage");
+					result.Add("Halls_of_Pain");
+					result.Add("Glacial_Trail");
+					result.Add("Frozen_Tundra");
+					result.Add("The_Ancients_Way");
+					result.Add("Worldstone_Keep_level_2");
+					break;
+			}
+			return result;
+		}
 	}
 	public enum Act : int
 	{
